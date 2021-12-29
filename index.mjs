0c7da955238e8bb44ed34d85a25633271ba276dd
Adam <adamgensh@gmail.com>
2021-12-29 13:59:46 +0200

    Initial fixes

--- index.mjs
+++ index.mjs
@@ -1,15 +1,12 @@
+#!/usr/bin/env node
+
 import 'zx/globals';
 import { program } from 'commander';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import { resetDir } from './lib/reset-dir.mjs';
 import { addRemotes } from './lib/add-remotes.mjs';
 import { gitFetch } from './lib/git-fetch.mjs';
 import { addBranchPerRepo } from './lib/add-branch-per-repo.mjs';
 import { mergeRepos } from './lib/merge-repos.mjs';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 program.version('0.0.1');
 
@@ -17,12 +14,22 @@ program
     .command('merge')
     .requiredOption('-r, --repos [string...]', 'Repository url')
     .requiredOption('-d, --dir [string]', 'Destination directory')
-    .option('--reset-dir', 'Clear the destination directory before running')
+    .option('--reset-dir', 'Clear the destination directory before running. Very useful for debugging.')
     .description('describe')
-    .action(async ({ repos, dir, isResetDir }) => {
-        const destinationDir = path.join(__dirname, dir);
+    .action(async ({ repos, dir, resetDir: isResetDir }) => {
+        const pwd = (await $`pwd`).toString().trim();
+        const destinationDir = path.join(pwd, dir);
 
-        await resetDir(destinationDir, isResetDir);
+        if (fs.existsSync(destinationDir)) {
+            if (isResetDir) {
+                await resetDir(destinationDir, isResetDir);
+            } else {
+                throw new Error(`Path exists on ${destinationDir}`);
+            }
+        } else {
+            await resetDir(destinationDir, isResetDir);
+        }
+
         await addRemotes(destinationDir, repos);
         await gitFetch(destinationDir, repos);
         await addBranchPerRepo(destinationDir, repos);
