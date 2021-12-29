19190eeccba1a82304a87cba602e8b847e91d01a
Adam <adamgensh@gmail.com>
2021-12-29 14:17:18 +0200

    Add question to validate repos

--- index.mjs
+++ index.mjs
@@ -7,6 +7,7 @@ import { addRemotes } from './lib/add-remotes.mjs';
 import { gitFetch } from './lib/git-fetch.mjs';
 import { addBranchPerRepo } from './lib/add-branch-per-repo.mjs';
 import { mergeRepos } from './lib/merge-repos.mjs';
+import { approveRepos } from './lib/validate-repos.mjs';
 
 program.version('0.0.1');
 
@@ -14,11 +15,18 @@ program
     .command('merge')
     .requiredOption('-r, --repos [string...]', 'Repository url')
     .requiredOption('-d, --dir [string]', 'Destination directory')
-    .option('--reset-dir', 'Clear the destination directory before running. Very useful for debugging.')
+    .option(
+        '--reset-dir',
+        'Clear the destination directory before running. Very useful for debugging.',
+    )
     .description('describe')
     .action(async ({ repos, dir, resetDir: isResetDir }) => {
         const pwd = (await $`pwd`).toString().trim();
         const destinationDir = path.join(pwd, dir);
+
+        if (!(await approveRepos(repos))) {
+            process.exit();
+        }
 
         if (fs.existsSync(destinationDir)) {
             if (isResetDir) {
