af3eed98ebc029e087e58bae0bcf5c96634b04ca
Adam <adamgensh@gmail.com>
2021-12-29 13:58:52 +0200

    prettier on index.mjs

--- index.mjs
+++ index.mjs
@@ -1,33 +1,32 @@
-import 'zx/globals'
-import {program} from 'commander';
-import {fileURLToPath} from 'url'
-import {dirname} from 'path'
-import {resetDir} from "./lib/reset-dir.mjs";
-import {addRemotes} from "./lib/add-remotes.mjs";
-import {gitFetch} from "./lib/git-fetch.mjs";
-import {addBranchPerRepo} from "./lib/add-branch-per-repo.mjs";
-import {mergeRepos} from "./lib/merge-repos.mjs";
+import 'zx/globals';
+import { program } from 'commander';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { resetDir } from './lib/reset-dir.mjs';
+import { addRemotes } from './lib/add-remotes.mjs';
+import { gitFetch } from './lib/git-fetch.mjs';
+import { addBranchPerRepo } from './lib/add-branch-per-repo.mjs';
+import { mergeRepos } from './lib/merge-repos.mjs';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 program.version('0.0.1');
 
-
-program.command('merge')
+program
+    .command('merge')
     .requiredOption('-r, --repos [string...]', 'Repository url')
     .requiredOption('-d, --dir [string]', 'Destination directory')
     .option('--reset-dir', 'Clear the destination directory before running')
     .description('describe')
-    .action(async ({repos, dir, isResetDir}) => {
+    .action(async ({ repos, dir, isResetDir }) => {
         const destinationDir = path.join(__dirname, dir);
 
-        await resetDir(destinationDir, isResetDir)
-        await addRemotes(destinationDir, repos)
-        await gitFetch(destinationDir, repos)
-        await addBranchPerRepo(destinationDir, repos)
-        await mergeRepos(destinationDir, repos)
-
+        await resetDir(destinationDir, isResetDir);
+        await addRemotes(destinationDir, repos);
+        await gitFetch(destinationDir, repos);
+        await addBranchPerRepo(destinationDir, repos);
+        await mergeRepos(destinationDir, repos);
     });
 
 program.parse();
