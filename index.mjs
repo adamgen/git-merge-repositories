3757f4edc534a7f9489ca998c777c704072c41a0
Adam <adamgensh@gmail.com>
2021-12-29 12:31:13 +0200

    Init

---
+++ index.mjs
@@ -0,0 +1,33 @@
+import 'zx/globals'
+import {program} from 'commander';
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import {resetDir} from "./lib/reset-dir.mjs";
+import {addRemotes} from "./lib/add-remotes.mjs";
+import {gitFetch} from "./lib/git-fetch.mjs";
+import {addBranchPerRepo} from "./lib/add-branch-per-repo.mjs";
+import {mergeRepos} from "./lib/merge-repos.mjs";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+program.version('0.0.1');
+
+
+program.command('merge')
+    .requiredOption('-r, --repos [string...]', 'Repository url')
+    .requiredOption('-d, --dir [string]', 'Destination directory')
+    .option('--reset-dir', 'Clear the destination directory before running')
+    .description('describe')
+    .action(async ({repos, dir, isResetDir}) => {
+        const destinationDir = path.join(__dirname, dir);
+
+        await resetDir(destinationDir, isResetDir)
+        await addRemotes(destinationDir, repos)
+        await gitFetch(destinationDir, repos)
+        await addBranchPerRepo(destinationDir, repos)
+        await mergeRepos(destinationDir, repos)
+
+    });
+
+program.parse();
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+dev
+
+```
+nodemon --watch index.mjs --watch lib index.mjs merge --repos git@github.com:adamgen/adamgen-blog.git git@github.com:adamgen/website.git --dir core --reset-dir
+```
